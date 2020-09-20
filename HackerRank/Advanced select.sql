0ed52cd24b4631a3f6838c75102b6d9cc06c0675
Huizhe (Sunny) Zhu <56517610+hz2657@users.noreply.github.com>
2020-09-20 00:13:17 -0400

    Update Advanced select.sql

--- HackerRank/Advanced select.sql
+++ HackerRank/Advanced select.sql
@@ -1,3 +1,28 @@
 - The PADS
 
+SELECT CONCAT(Name, '(',  LEFT(Occupation, 1), ')') AS Names
+FROM OCCUPATIONS
+ORDER BY Names;
+
+SELECT CONCAT('There are a total of ',  COUNT(Occupation), ' ',  lower(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation), Occupation;
+
+
+- Type of Triangle
+SELECT 
+CASE 
+   WHEN (A+B>C AND A+C>B AND B+C>A) THEN 
+        CASE 
+            WHEN A=B AND B=C THEN 'Equilateral'
+            WHEN A <> B AND B <> C AND A <>C THEN 'Scalene'
+            ELSE 'Isosceles' 
+        END
+   ELSE 'Not A Triangle'
+END
+FROM TRIANGLES;
+
+
+
 
