d44478dffad7dac636d881d98977f0960be7c430
Huizhe (Sunny) Zhu <56517610+hz2657@users.noreply.github.com>
2020-09-21 02:13:03 -0400

    Update Aggregation.sql

--- HackerRank/Aggregation.sql
+++ HackerRank/Aggregation.sql
@@ -5,7 +5,7 @@ SELECT CEIL(AVG(Salary) - AVG(REPLACE(Salary, 0, '')))
 FROM EMPLOYEES 
 
 
----Top Earners
+                                      ---Top Earners
 ----GROUP BY 1
 SELECT (months*salary) AS total_salary, COUNT(*)
 FROM Employee 
@@ -13,3 +13,15 @@ GROUP BY 1
 ORDER BY total_salary DESC
 LIMIT 1;
 
+---Weather Observation Station 2
+------ SUM FIRST THEN ROUND    
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) 
+FROM STATION;
+                                     
+---Weather Observation Station 14
+----Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to  decimal places.
+SELECT ROUND(MAX(LAT_N),4)
+FROM STATION
+WHERE LAT_N < 137.2345;
+
+                                      
