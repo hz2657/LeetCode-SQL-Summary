e3672c16dda181c57bec32870ca6cac1478242af
Huizhe (Sunny) Zhu <56517610+hz2657@users.noreply.github.com>
2020-09-21 12:56:17 -0400

    Update Aggregation.sql

--- HackerRank/Aggregation.sql
+++ HackerRank/Aggregation.sql
@@ -23,4 +23,29 @@ SELECT ROUND(MAX(LAT_N),4)
 FROM STATION
 WHERE LAT_N < 137.2345;
 
+---Weather Observation Station 15
+------WHERE _IN ()
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N IN (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+                                      
+---Weather Observation Station 17
+SELECT ROUND(LONG_W,4)
+FROM STATION 
+WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N >38.7780)
+
+---Weather Observation Station 18
+---- Manhattan Distance: The distance between two points measured along axes at right angles. In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|.
+SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)),4)
+FROM STATION; 
+                                    
+---Weather Observation Station 19
+---- Euclidean distance is the "ordinary" straight-line distance between two points in Euclidean space  
+---- POWER(___, 2): SQUARE                                               
+SELECT ROUND( SQRT(POWER(MIN(LAT_N) - MAX(LAT_N),2) +POWER(MIN(LONG_W) - MAX(LONG_W),2)), 4)
+FROM STATION;                                              
+                       
+                   
+                   
+                                      
                                       
