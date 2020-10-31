e3b811516c11625cca2bb86df9b749cf93af198f
Huizhe (Sunny) Zhu <56517610+hz2657@users.noreply.github.com>
2020-10-31 11:10:31 -0400

    Update 626. Exchange Seats.sql

--- Leetcode/626. Exchange Seats.sql
+++ Leetcode/626. Exchange Seats.sql
@@ -5,3 +5,13 @@ CASE
     ELSE student
 END AS student   # VERY important
 FROM seat s1;
+
+ #---or
+                               
+SELECT S1.id, 
+CASE 
+    WHEN S1.id%2 = 0 THEN (SELECT student FROM seat S2 WHERE S2.id=S1.id-1)
+    WHEN S1.id = (SELECT MAX(id) FROM seat) THEN student
+    ELSE (SELECT student FROM seat S3 WHERE S3.id=S1.id+1)
+END AS student 
+FROM seat S1;
