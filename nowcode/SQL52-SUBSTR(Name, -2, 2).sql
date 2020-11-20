ebd587c4431de647d759d28db8459354237ed3b8
Huizhe (Sunny) Zhu <56517610+hz2657@users.noreply.github.com>
2020-11-19 21:48:53 -0500

    Create SQL52-SUBSTR(Name, -2, 2).sql

---
+++ nowcode/SQL52-SUBSTR(Name, -2, 2).sql
@@ -0,0 +1,19 @@
+
+
+
+SELECT first_name
+FROM Employees
+ORDER BY SUBSTR(first_name, -2, 2); 
+
+
+
+# check this: https://www.w3schools.com/sql/func_mysql_substr.asp
+
+# more examples: 
+
+# SELECT SUBSTR(CustomerName, 2, 5) AS ExtractString
+
+# SELECT SUBSTR("SQL Tutorial", -5, 5) AS ExtractString;
+
+
+# good luck! 
